4b1319b10b4ef060f6651917326350040e0feb05
Y_BlueMond <trrd4480@gmail.com>
2023-01-31 01:13:24 +0000

    update

---
+++ public/login/js/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAv2pC3rhqDDTGxOMSOj3haGjvNH5zILAk",
+    authDomain: "ybluemond.firebaseapp.com",
+    databaseURL: "https://ybluemond-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ybluemond",
+    storageBucket: "ybluemond.appspot.com",
+    messagingSenderId: "548956595822",
+    appId: "1:548956595822:web:d2b3fdee5ca359ff49687f",
+    measurementId: "G-88S1HQBXW9"
+  };
